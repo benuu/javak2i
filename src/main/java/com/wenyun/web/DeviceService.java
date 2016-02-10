9f7a4d3199e4958e1670abd3c1cc82bce4e4dc35
wshi <wshi@splunk.com>
2016-02-10 17:05:09 +0800

    update

--- src/main/java/com/wenyun/web/DeviceService.java
+++ src/main/java/com/wenyun/web/DeviceService.java
@@ -4,14 +4,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-@Path("/ps")
+@Path("/ds")
 public class DeviceService {
 	
 	@GET
 	@Path("/test")
 	public Response psTest() {
  
-		String output = "This is pump service test function";
+		String output = "This is device service test function";
  
 		return Response.status(200).entity(output).build();
  
